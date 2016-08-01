2e269b07ae95e765502e721c10455b823dcfa325
zrqandroid <563325274@qq.com>
2016-08-01 17:21:50 +0800

    store factory

---
+++ app/src/main/java/com/maowubian/baseproject/api/App.java
@@ -0,0 +1,60 @@
+package com.maowubian.baseproject.api;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.maowubian.baseproject.factory.StoreConfigFactory;
+import com.orhanobut.logger.Logger;
+
+
+/**
+ * Created by zhuruqiao on 16/8/1.
+ */
+public class App extends Application {
+
+    public static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext=this;
+        Logger.i("path:%s", StoreConfigFactory.getStorePath(StoreConfigFactory.DIR_HTTP));
+
+    }
+
+//    private void initOkhttp() {
+//        long httpCacheSize = StoreConfigFactory.configStoreLimitSize(StoreConfigFactory.DIRECTORY_HTTP);
+//        File httpCacheDirectory = new File(StoreConfigFactory.getStorePath(StoreConfigFactory.DIRECTORY_HTTP));
+//        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .connectTimeout(30, TimeUnit.SECONDS)
+//                .readTimeout(60,TimeUnit.SECONDS)
+//                .writeTimeout(60,TimeUnit.SECONDS)
+//                .followRedirects(true)
+//                .retryOnConnectionFailure(true)
+//                .addNetworkInterceptor(new UsualRequestInterceptor())
+//                .cache(new Cache(httpCacheDirectory, httpCacheSize))
+//                .authenticator(new Authenticator() {
+//                    @Override
+//                    public Request authenticate(Route route, Response response) throws IOException {
+//                        AccountManager.get().invalidateToken();
+//                        try {
+//                            String token = AccountManager.get().getToken();
+//                            return response.request().newBuilder().header("token", token).build();
+//                        } catch (Exception ignored) {
+//
+//                        }
+//                        return response.request();
+//                    }
+//                })
+//                .build();
+//        mObjectMapper = configObjectMapper();
+//        JacksonConverterFactory factory = JacksonConverterFactory.create(mObjectMapper);
+//        Retrofit retrofit = new Retrofit.Builder().baseUrl(Constants.BASE_URL)
+//                .client(okHttpClient)
+//                .addConverterFactory(factory)
+//                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+//                .build();
+//
+//        service = retrofit.create(MarketService.class);
+//    }
+}
---
+++ app/src/main/java/com/maowubian/baseproject/api/RequestService.java
@@ -0,0 +1,7 @@
+package com.maowubian.baseproject.api;
+
+/**
+ * Created by zhuruqiao on 16/8/1.
+ */
+public interface RequestService {
+}
